b8a8e42494328365e5436ac7a361d4433350287b
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-11-30 15:14:03 -0800

    Add Conditionals, Arrays and Objects warm up.

--- src/warm-up.js
+++ src/warm-up.js
@@ -53,14 +53,22 @@ console.log(isShortStory);
 // Express the following in code: If the variable "pages" is less than 1000 log
 // the statement 'I could read that in a day!', otherwise, log the statement 
 // 'Ok, maybe I could read that in a day and a half'
-
+if (pages < 1000) {
+    console.log('I could read that in a day!')
+} else {
+    console.log('Ok, maybe I could read that in a day and a half')
+};
 
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
-
+if (isFavorite) {
+    console.log(`My favorite book is ${title}!`)
+} else {
+    console.log(`I recently read the ${title} book.`)
+};
 
 
 //------------------------- Arrays -------------------------//
@@ -68,25 +76,32 @@ console.log(isShortStory);
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
-
+var books = ['Bloodhound', 'Mastiff', 'Oath of Fealty', 'Joust', 'Arie'];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+books[2];
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
+var belowTwoHundred = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+belowTwoHundred[4];
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable "dictionary" and assign to it an object literal with two
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
-
+var dictionary = {
+    name: "Webster's",
+    hasThesaurus: false
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+dictionary.pageCount = 720;
+console.log(dictionary);
