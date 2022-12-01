1270919e2e21729640dd1b1b1bdc1f40a84c17db
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-11-30 17:12:22 -0800

    Add functions to book.js to test.

--- src/book.js
+++ src/book.js
@@ -1,11 +1,34 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return character;
+};
+
+
+function saveReview(review, array) { 
+  if (array.includes(review) === false) {
+    array.push(review);
+  };
+};
+
+
+// function calculatePageCount() {
+
+// };
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
