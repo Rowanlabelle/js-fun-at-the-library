132625a658a92e3f4ef17e85075f96831cb4c8af
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-01 20:03:04 -0800

    Add shelf iteration and test.

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
+function shelfBook(book, shelf) {
+  if (shelf.length  < 3) { 
+    if (book.genre === 'sciFi') {
+      shelf.unshift(book);
+    };
+  };
+};
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === book) {
+      shelf.splice(i,1);
+      return shelf;
+    }; 
+  };
+};
+
+function listTitles(books) {
+  var allTitles = [];
+  for (var i = 0; i < books.length; i++) {
+    allTitles.push(books[i].title);  
+  };
+  return allTitles.join(', ');
+};
+
+function searchShelf(shelf, book) {
+  var result = undefined;
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title !== book) {
+      result = false;
+    } else {
+      result = true;
+    };
+  };
+  return result;
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
