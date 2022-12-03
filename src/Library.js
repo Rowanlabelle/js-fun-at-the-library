6f6eeb48d35a5d0aa85ad237c1193e4bf4ea79ab
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-02 18:14:04 -0800

    Add Library iteration and library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,33 @@
+function createLibrary(libName) {
+  var library = {
+    name: libName,
+    shelves: {
+        fantasy: [],
+        fiction: [],
+        nonFiction: []
+    }
+  };
+  return library;
+};
 
+function addBook(library, book) {
+  var bookGen = book.genre;
+  library['shelves'][bookGen].push(book);
+};
+
+function checkoutBook(library, bookTitle, genre) {
+  var findShelf = library['shelves'][genre];
+  for (var i = 0; i < findShelf.length; i++) {
+    if (bookTitle === findShelf[i].title) {
+      findShelf.splice(i, 1);
+      return `You have now checked out ${bookTitle} from the ${library.name}`
+    }
+  };
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`;
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
