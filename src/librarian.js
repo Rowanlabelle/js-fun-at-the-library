f6ad92de4f437b7e548f01812af5cfbdd7741b47
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-04 18:45:35 -0800

    Add librarian and librarian test iterations

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,38 @@
 class Librarian {
-  constructor() {
-    
-  }
-}
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
+  
+  greetPatron(name, morning) {
+    if (morning === true) {
+      return `Good morning, ${name}!`
+    } else {
+      return `Hello, ${name}!`
+    };
+  };
+
+  findBook(bookName) {
+    var shelfGenres = Object.values(this.library.shelves);
+    for (var i = 0; i < shelfGenres.length; i++) {
+      var theTitle = shelfGenres[i][i].title;
+      var shelf = shelfGenres[i];
+      if (bookName === theTitle) {
+        for (var j = 0; j < theTitle.length; j++) {
+          shelf.splice(j, 1);
+          return `Yes, we have ${bookName}`
+        };
+      };
+      if (bookName !== theTitle) {
+        return `Sorry, we do not have ${bookName}`;
+      };
+    };
+  };
+
+  calculateLateFee(days) {
+    return Math.ceil(days * .25); 
+  };
+
+};
 
 module.exports = Librarian;
